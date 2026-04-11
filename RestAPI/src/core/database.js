c71bf823cd51574140df5bbbb69b9cb4934731d8
wilson <wilsonesmabe2003@gmail.com>
2026-04-11 14:41:56 +0800

    added REST API to use it as  boilerplate soon

---
+++ RestAPI/src/core/database.js
@@ -0,0 +1,17 @@
+import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+
+// Load environment variables from .env
+dotenv.config();
+
+
+const connection = await mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
+});
+
+export { connection };
+
